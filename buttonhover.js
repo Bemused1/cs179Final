8ff3c7d5fe76d4e3f91e6feeaffaec2ea9a24bdd
Rahul Naidoo <rahulnaidoo@college.harvard.edu>
2018-04-11 23:14:06 -0400

    added bad button highlighting, will fix highlighting to look more professional, and make it consistent, before the next testing round

---
+++ buttonhover.js
@@ -0,0 +1,11 @@
+
+
+function onHover() {
+	var btn = document.getElementById("btn");
+	btn.src = "icons/submit.png";
+};
+
+function offHover() {
+	var btn = document.getElementById("btn");
+	btn.src = "icons/unselectedsubmit.png";
+};
